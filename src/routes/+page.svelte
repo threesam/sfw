f83ed3f38f57002514d61176a018d494f7f24bd6
Sam <57677512+threesam@users.noreply.github.com>
2023-05-05 13:44:01 -0400

    cleans hp styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,8 +14,10 @@
 
 <section class="grid h-96 w-full place-content-center bg-gradient-to-tl from-gray-700 to-gray-900">
 	<div class="flex max-w-3xl flex-col items-center gap-5 p-5 lg:grid lg:grid-cols-2">
-		<h2 class="bg-dark flex h-full w-full items-center justify-center p-5 text-3xl font-bold">
-			Mission Statement
+		<h2
+			class="font-display flex h-full w-full items-center justify-center border p-5 text-5xl font-bold"
+		>
+			Our Mission
 		</h2>
 		<p class="text-center lg:text-left">
 			Magna eiusmod qui in voluptate eu proident dolor magna officia esse nostrud do. Id cupidatat
@@ -26,10 +28,15 @@
 
 <Swiper title="Films" slides={projects.slice(1)} />
 
+<h4 class="font-display px-5 text-4xl lg:px-10">Merch</h4>
 <div class="bg-dark mb-10 grid gap-10 p-5 lg:grid-cols-3 lg:gap-2 lg:p-10">
 	{#each products as product}
 		<div class="">
-			<img class="max-w-full bg-gradient-to-tr from-slate-100" src={product.thumbnail_url} alt="" />
+			<img
+				class="mb-2 max-w-full bg-gradient-to-tr from-slate-100"
+				src={product.thumbnail_url}
+				alt=""
+			/>
 			<h4>{product.name}</h4>
 			<p>{product.variants[0].retail_price} - <span>{product.variants[0].currency}</span></p>
 		</div>
