55fc335992a9c58d47c70fa0c94c8234f1450bfb
Sam <57677512+threesam@users.noreply.github.com>
2023-05-07 00:17:50 -0400

    mobile footer styles

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -20,7 +20,9 @@
 </script>
 
 {#if route !== '/contact'}
-	<footer class="bg-primary flex flex-col items-center justify-between gap-10 p-10 lg:flex-row">
+	<footer
+		class="bg-primary flex flex-col items-center justify-between gap-5 p-5 lg:flex-row lg:gap-10 lg:p-10"
+	>
 		<SubscribeForm />
 		<SocialLinks links={settings.links} size={69} />
 	</footer>
