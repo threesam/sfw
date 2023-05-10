436372b8efd566ac1643e742d45738f3d30b36f3
Sam <57677512+threesam@users.noreply.github.com>
2023-05-10 14:22:23 -0400

    adds stripe routes

--- src/routes/checkout/payment-intent/+server.ts
+++ src/routes/checkout/payment-intent/+server.ts
@@ -1,21 +1,16 @@
-import Stripe from 'stripe'
-import { SECRET_STRIPE_KEY } from '$env/static/private'
+import { createCheckoutSession } from '$utils/stripe'
 
-const stripe = new Stripe(SECRET_STRIPE_KEY)
+export async function POST({ request }) {
+	const items = await request.json()
+	const { origin } = new URL(request.url)
 
-export async function GET({ request }) {
-	const session = await stripe.checkout.sessions.create({
-		line_items: [
-			{
-				// Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-				price: 'price_1Ka3JRENbNtMYgqEvxU4sKE5',
-				quantity: 1
-			}
-		],
-		mode: 'payment',
-		success_url: `https://whocares.com/success.html`,
-		cancel_url: `https://whocares.com/cancel.html`
-	})
+	try {
+		const session = await createCheckoutSession({ origin, items })
 
-	return new Response(JSON.stringify({ session }, null, 2))
+		return new Response(session.url)
+	} catch (error) {
+		return new Response(error.message, {
+			status: error.status
+		})
+	}
 }
---
+++ src/routes/checkout/success/+page.svelte
@@ -0,0 +1,3 @@
+<h1>Thanks for your order</h1>
+
+<a href="/">back home</a>
