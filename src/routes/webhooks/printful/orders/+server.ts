14f3901844e6fa3ed067c17207e3df393fdd5149
Sam <57677512+threesam@users.noreply.github.com>
2023-05-12 23:47:19 -0400

    fixes formatting error

--- src/routes/webhooks/printful/orders/+server.ts
+++ src/routes/webhooks/printful/orders/+server.ts
@@ -1,107 +1,107 @@
 import { env } from '$env/dynamic/private'
-import Stripe from "stripe";
+import Stripe from 'stripe'
 
 type CustomerDetails = {
-  address: {
-    city: string;
-    country: string;
-    line1: string;
-    line2: string | null;
-    postal_code: string;
-    state: string;
-  };
-  email: string;
-  name: string;
-  phone: string | null;
-  tax_exempt: string;
-  tax_ids: string[];
-};
+	address: {
+		city: string
+		country: string
+		line1: string
+		line2: string | null
+		postal_code: string
+		state: string
+	}
+	email: string
+	name: string
+	phone: string | null
+	tax_exempt: string
+	tax_ids: string[]
+}
 
 type LineItem = {
-  id: string;
-  object: "price";
-  active: boolean;
-  billing_scheme: string;
-  created: number;
-  currency: string;
-  custom_unit_amount: string | null;
-  livemode: boolean;
-  lookup_key: string | null;
-  metadata: any;
-  nickname: string | null;
-  product: string;
-  recurring: null;
-  tax_behavior: string;
-  tiers_mode: null;
-  transform_quantity: null;
-  type: string;
-  unit_amount: number;
-  unit_amount_decimal: string;
-  quantity: number;
-};
+	id: string
+	object: 'price'
+	active: boolean
+	billing_scheme: string
+	created: number
+	currency: string
+	custom_unit_amount: string | null
+	livemode: boolean
+	lookup_key: string | null
+	metadata: any
+	nickname: string | null
+	product: string
+	recurring: null
+	tax_behavior: string
+	tiers_mode: null
+	transform_quantity: null
+	type: string
+	unit_amount: number
+	unit_amount_decimal: string
+	quantity: number
+}
 
-export async function POST({request}) {
-  // @ts-expect-error it is what it is
-  const stripe = new Stripe(env.STRIPE_TEST_SECRET_KEY);
+export async function POST({ request }) {
+	// @ts-expect-error it is what it is
+	const stripe = new Stripe(env.STRIPE_TEST_SECRET_KEY)
 
-  // get stripe signature
-  const signature = request.headers.get("Stripe-Signature") ?? "";
+	// get stripe signature
+	const signature = request.headers.get('Stripe-Signature') ?? ''
 
-  // validate that the webhook originated from Stripe
-  const { data } = await stripe.webhooks.constructEventAsync(
-    await request.text(), // raw request body
-    signature, // signature header
-    env.STRIPE_WEBHOOK_SECRET
-  );
+	// validate that the webhook originated from Stripe
+	const { data } = await stripe.webhooks.constructEventAsync(
+		await request.text(), // raw request body
+		signature, // signature header
+		env.STRIPE_WEBHOOK_SECRET
+	)
 
-  // set customer info
-  const customer = data?.object?.customer_details as CustomerDetails;
+	// set customer info
+	const customer = data?.object?.customer_details as CustomerDetails
 
-  // get checkout line items
-  const { data: lineItems } = await stripe.checkout.sessions.listLineItems(
-    id: data?.object?.id
-  );
+	const checkoutId = data?.object?.id
 
-  // get price for each line item, include quantity
-  const items = (await Promise.all(
-    lineItems.map(
-      (item) =>
-        item.price?.id &&
-        stripe.prices.retrieve(item.price?.id).then((result) => {
-          return {
-            ...result,
-            quantity: item.quantity,
-          };
-        })
-    )
-  )) as LineItem[];
+	// get checkout line items
+	const { data: lineItems } = await stripe.checkout.sessions.listLineItems(checkoutId)
 
-  // pass line items and customer details to printful order api
-  const printfulOrderRes = await fetch("https://api.printful.com/orders", {
-    headers: {
-      Authorization: "Bearer " + env.PRINTFUL_PRIVATE_ACCESS_KEY,
-    },
-    method: "POST",
-    body: JSON.stringify({
-      recipient: {
-        name: customer.name,
-        address1: customer.address.line1,
-        city: customer.address.city,
-        state_code: customer.address.state,
-        country_code: customer.address.country,
-        zip: customer.address.postal_code,
-      },
-      items: items.map((item) => ({
-        external_variant_id: item.product.split("_").pop(),
-        quantity: item.quantity,
-      })),
-    }),
-  });
+	// get price for each line item, include quantity
+	const items = (await Promise.all(
+		lineItems.map(
+			(item) =>
+				item.price?.id &&
+				stripe.prices.retrieve(item.price?.id).then((result) => {
+					return {
+						...result,
+						quantity: item.quantity
+					}
+				})
+		)
+	)) as LineItem[]
 
-  // get result of order request
-  const { result } = await printfulOrderRes.json();
+	// pass line items and customer details to printful order api
+	const printfulOrderRes = await fetch('https://api.printful.com/orders', {
+		headers: {
+			Authorization: 'Bearer ' + env.PRINTFUL_PRIVATE_ACCESS_KEY
+		},
+		method: 'POST',
+		body: JSON.stringify({
+			recipient: {
+				name: customer.name,
+				address1: customer.address.line1,
+				city: customer.address.city,
+				state_code: customer.address.state,
+				country_code: customer.address.country,
+				zip: customer.address.postal_code
+			},
+			items: items.map((item) => ({
+				external_variant_id: item.product.split('_').pop(),
+				quantity: item.quantity
+			}))
+		})
+	})
 
-  console.log({ data, result, customer, items });
+	// get result of order request
+	const { result } = await printfulOrderRes.json()
 
-  return new Response("ok");
+	console.log({ data, result, customer, items })
+
+	return new Response('ok')
 }
--- src/routes/webhooks/printful/products/+server.ts
+++ src/routes/webhooks/printful/products/+server.ts
@@ -25,66 +25,62 @@ function getId({ product, variant }) {
 }
 
 export async function POST({ request }) {
-	// STRIPE UTILS
-	// @ts-expect-error it is what it is
+	const webhookData: PrintfulWebhook = await request.json()
+
+	// @ts-expect-error init stripe
 	const stripe = new Stripe(env.STRIPE_TEST_SECRET_KEY)
 
-	if (request.method === 'POST') {
-		const webhookData: PrintfulWebhook = await request.json()
+	const product = await getProduct({
+		id: webhookData.data.sync_product.id
+	})
 
-		const product = await getProduct({
-			id: webhookData.data.sync_product.id
-		})
+	const stripeProducts = await stripe.products.list()
 
-		const stripeProducts = await stripe.products.list()
+	if (webhookData.type === 'product_updated') {
+		// loop through variants and upsert to stripe
+		const variants = await Promise.all(
+			product?.variants?.map((variant) => {
+				// check if product already exists
+				const productExists = stripeProducts.data.find((p) => p.id === getId({ product, variant }))
 
-		if (webhookData.type === 'product_updated') {
-			// loop through variants and upsert to stripe
-			const variants = await Promise.all(
-				product?.variants?.map((variant) => {
-					// check if product already exists
-					const productExists = stripeProducts.data.find(
-						(p) => p.id === getId({ product, variant })
-					)
-
-					// if it exists, update it
-					if (productExists) {
-						return stripe.products.update(getId({ product, variant }), {
-							name: variant.name,
-							images: product.thumbnail_url ? [product.thumbnail_url] : []
-						})
-					}
-
-					// otherwise create a new one
-					return stripe.products.create({
-						id: getId({ product, variant }),
+				// if it exists, update it
+				if (productExists) {
+					return stripe.products.update(getId({ product, variant }), {
 						name: variant.name,
-						images: product.thumbnail_url ? [product.thumbnail_url] : [],
-						default_price_data: {
-							currency: variant.currency,
-							unit_amount: +variant.retail_price * 100
-						}
+						images: product.thumbnail_url ? [product.thumbnail_url] : []
 					})
+				}
+
+				// otherwise create a new one
+				return stripe.products.create({
+					id: getId({ product, variant }),
+					name: variant.name,
+					images: product.thumbnail_url ? [product.thumbnail_url] : [],
+					default_price_data: {
+						currency: variant.currency,
+						unit_amount: +variant.retail_price * 100
+					}
 				})
-			)
+			})
+		)
 
-			return new Response(JSON.stringify(variants, null, 2))
-		}
-
-		if (webhookData.type === 'product_deleted') {
-			// parent product id
-			const productId = webhookData.data.sync_product.external_id
-
-			// return products that include parent product id
-			const productsToDelete = stripeProducts.data.filter((p) => p.id.split('_')[1] === productId)
-
-			// loop through products, set to inactive
-			for (const p of productsToDelete) {
-				await stripe.products.update(p.id, { active: false })
-			}
-
-			return new Response('Product(s) set to inactive')
-		}
+		return new Response(JSON.stringify(variants, null, 2))
 	}
+
+	if (webhookData.type === 'product_deleted') {
+		// parent product id
+		const productId = webhookData.data.sync_product.external_id
+
+		// return products that include parent product id
+		const productsToDelete = stripeProducts.data.filter((p) => p.id.split('_')[1] === productId)
+
+		// loop through products, set to inactive
+		for (const p of productsToDelete) {
+			await stripe.products.update(p.id, { active: false })
+		}
+
+		return new Response('Product(s) set to inactive')
+	}
+
 	return new Response('Unsupported webhook')
 }
