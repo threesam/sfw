509a184f5603f8ce0037da4b60e85d1b9771b7c7
Sam <57677512+threesam@users.noreply.github.com>
2023-05-12 23:23:06 -0400

    consolidates webhook

---
+++ src/routes/webhooks/printful/+server.ts
@@ -0,0 +1,90 @@
+import { env } from '$env/dynamic/private'
+import Stripe from 'stripe'
+import { getProduct } from '$utils/printful.js'
+
+type PrintfulWebhook = {
+	type: 'product_updated' | 'product_deleted'
+	created: number
+	retries: number
+	store: number
+	data: {
+		sync_product: {
+			id: number
+			external_id: string
+			name: string
+			variants?: number
+			synced?: number
+			thumbnail_url?: string
+			is_ignored?: boolean
+		}
+	}
+}
+
+function getId({ product, variant }) {
+	return `printful_${product.external_id}_${variant.external_id}`
+}
+
+export async function POST({ request }) {
+	// STRIPE UTILS
+	// @ts-expect-error it is what it is
+	const stripe = new Stripe(env.TEST_STRIPE_KEY)
+
+	if (request.method === 'POST') {
+		const webhookData: PrintfulWebhook = await request.json()
+
+		const product = await getProduct({
+			id: webhookData.data.sync_product.id
+		})
+
+		const stripeProducts = await stripe.products.list()
+
+		if (webhookData.type === 'product_updated') {
+			// loop through variants and upsert to stripe
+			const variants = await Promise.all(
+				product?.variants?.map((variant) => {
+					// check if product already exists
+					const productExists = stripeProducts.data.find(
+						(p) => p.id === getId({ product, variant })
+					)
+
+					// if it exists, update it
+					if (productExists) {
+						return stripe.products.update(getId({ product, variant }), {
+							name: variant.name,
+							images: product.thumbnail_url ? [product.thumbnail_url] : []
+						})
+					}
+
+					// otherwise create a new one
+					return stripe.products.create({
+						id: getId({ product, variant }),
+						name: variant.name,
+						images: product.thumbnail_url ? [product.thumbnail_url] : [],
+						default_price_data: {
+							currency: variant.currency,
+							unit_amount: +variant.retail_price * 100
+						}
+					})
+				})
+			)
+
+			return new Response(JSON.stringify(variants, null, 2))
+		}
+
+		if (webhookData.type === 'product_deleted') {
+			// parent product id
+			const productId = webhookData.data.sync_product.external_id
+
+			// return products that include parent product id
+			const productsToDelete = stripeProducts.data.filter((p) => p.id.split('_')[1] === productId)
+
+			// loop through products, set to inactive
+			for (const p of productsToDelete) {
+				await stripe.products.update(p.id, { active: false })
+			}
+
+			return new Response('Product(s) set to inactive')
+		}
+	}
+	return new Response('Unsupported webhook')
+}
