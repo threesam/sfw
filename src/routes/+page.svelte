15c8e1899789991d4abe75b9c1535fd06367e481
Sam <57677512+threesam@users.noreply.github.com>
2023-05-14 14:31:54 -0400

    adds alternating padding to products on hp

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -29,17 +29,19 @@
 	slides={projects.slice(1).filter(({ status }) => status !== 'pre-production')}
 />
 
-<h4 class="font-display px-5 text-4xl lg:px-10">Merch</h4>
-<div class="bg-dark mb-10 grid gap-10 p-5 lg:grid-cols-3 lg:gap-2 lg:p-10">
-	{#each products as product}
-		<a href="/merch/{product.id}">
+<h4 class="font-display p-5 text-4xl lg:px-10">Merch</h4>
+<div class="bg-dark mb-10 grid gap-10 lg:grid-cols-3 lg:gap-2 lg:px-10">
+	{#each products as product, i}
+		<a href="/merch/{product.id}" class={`${i % 2 === 0 ? 'pl-5' : 'pr-5'} lg:p-0`}>
 			<img
 				class="mb-2 max-w-full bg-gradient-to-tr from-slate-100"
 				src={product.thumbnail_url}
 				alt=""
 			/>
-			<h4>{product.name}</h4>
-			<p>{product.variants[0].retail_price} - <span>{product.variants[0].currency}</span></p>
+			<div class={`${i % 2 === 0 ? '' : 'pl-5'} lg:p-0`}>
+				<h4>{product.name}</h4>
+				<p>{product.variants[0].retail_price} - <span>{product.variants[0].currency}</span></p>
+			</div>
 		</a>
 	{/each}
 </div>
