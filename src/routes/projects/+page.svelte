3f3b1bc527e04e790415ce0b45a61da374fdeb7c
Sam <57677512+threesam@users.noreply.github.com>
2023-05-14 13:51:22 -0400

    removes poster dimensions for null sc

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -7,6 +7,7 @@
 	const { projects } = data.body
 </script>
 
+<h1 class="font-display w-full px-5 pb-0 pt-10 lg:text-center">All Projects</h1>
 <div class="flex flex-col pt-10 last:mb-20">
 	{#each projects as project, index}
 		<SideBySide {...project} {index} path="/projects/" />
