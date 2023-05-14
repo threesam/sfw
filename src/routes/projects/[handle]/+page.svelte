a681b39b6035365d141951ccbd38268e3f8aea7b
Sam <57677512+threesam@users.noreply.github.com>
2023-05-14 15:31:55 -0400

    removes padding from title

--- src/routes/projects/[handle]/+page.svelte
+++ src/routes/projects/[handle]/+page.svelte
@@ -17,7 +17,7 @@
 <!-- <Banner height="47vh" image={project.image} /> -->
 <section class="bg-gradient-3 relative mb-5 grid h-64 w-full place-content-center bg-black">
 	<div class="absolute inset-0 bg-black/20" />
-	<h1 class="relative z-0 pb-10 pt-5 text-center text-3xl lg:text-5xl">{project.title}</h1>
+	<h1 class="relative z-0 text-center text-3xl lg:text-5xl">{project.title}</h1>
 </section>
 <section class="p-5">
 	{#if project.status === 'pre-production' && backstageLink}
