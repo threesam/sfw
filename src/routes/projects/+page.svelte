7d8d4474d3b6db61790cba7d15d2103637af0994
Sam <57677512+threesam@users.noreply.github.com>
2023-05-14 14:52:38 -0400

    sorts projects

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -5,11 +5,20 @@
 	export let data: PageData
 
 	const { projects } = data.body
+
+	let sortedProjects = []
+	projects.forEach((project) => {
+		if (project.status === 'pre-production') {
+			sortedProjects.push(project)
+		} else {
+			sortedProjects.unshift(project)
+		}
+	})
 </script>
 
 <h1 class="font-display w-full px-5 pb-0 pt-10 lg:text-center">All Projects</h1>
 <div class="flex flex-col pt-10 last:mb-20">
-	{#each projects as project, index}
+	{#each sortedProjects as project, index}
 		<SideBySide {...project} {index} path="/projects/" />
 	{/each}
 </div>
