9aef843f1163d3caaba3ed208e2894e1ccfcf367
Sam <57677512+threesam@users.noreply.github.com>
2023-05-14 23:34:53 -0400

    adjusts mobile menu color bg

--- src/lib/components/HeaderMenu.svelte
+++ src/lib/components/HeaderMenu.svelte
@@ -16,13 +16,13 @@
 	on:click|self={() => {
 		$showMenu = false
 	}}
-	class="bg-dark/50 absolute inset-0 top-16 z-50 flex min-h-screen w-full justify-end overflow-hidden lg:hidden"
+	class="bg-dark/90 absolute inset-0 top-16 z-50 flex min-h-screen w-full justify-end overflow-hidden lg:hidden"
 >
 	<!-- MENU -->
 	<div
 		bind:clientHeight
 		in:fly={{ y: -clientHeight, opacity: 100, duration: 400, easing: quintInOut }}
-		class="bg-dark z-30 w-full p-5 md:w-1/2 lg:w-1/3"
+		class="bg-gradient-3 z-30 w-full p-5 md:w-1/2 lg:w-1/3"
 	>
 		<div class="flex w-full flex-col items-start gap-5 py-5">
 			{#each links as link, i (link.title)}
@@ -35,7 +35,7 @@
 					>
 						<a
 							href={link.href}
-							class={`font-display text-light rounded-lg text-2xl font-bold hover:opacity-100 ${
+							class={`font-display text-dark rounded-lg text-2xl font-thin hover:opacity-100 ${
 								currentRoute === link.href ? 'opacity-100' : 'opacity-75'
 							}`}>{link.title}</a
 						>
