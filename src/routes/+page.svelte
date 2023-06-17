e0790869ec056de8d3cf737f210841819dc0e801
Sam <57677512+threesam@users.noreply.github.com>
2023-06-16 20:32:00 -0400

    removes mission statement and merch from hp

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,7 +12,7 @@
 
 <Banner {...featuredProject} path="/projects/" />
 
-<section class="grid h-96 w-full place-content-center bg-gradient-to-tl from-gray-700 to-gray-900">
+<!-- <section class="grid h-96 w-full place-content-center bg-gradient-to-tl from-gray-700 to-gray-900">
 	<div class="flex max-w-3xl flex-col items-center gap-5 p-5 lg:grid lg:grid-cols-2">
 		<h2
 			class="font-display text-light bg-dark flex h-full w-full items-center justify-center border border-slate-700 p-5 text-3xl font-thin"
@@ -24,14 +24,14 @@
 			amet eiusmod ad. Minim do nisi fugiat labore sit cillum cupidatat anim esse excepteur sint.
 		</p>
 	</div>
-</section>
+</section> -->
 
 <Swiper
 	title="Featured Projects"
 	slides={projects.slice(1).filter(({ status }) => status !== 'pre-production')}
 />
 
-<h4 class="font-display p-5 text-4xl lg:px-10">Merch</h4>
+<!-- <h4 class="font-display p-5 text-4xl lg:px-10">Merch</h4>
 <div class="bg-dark mb-10 grid gap-10 lg:grid-cols-3 lg:gap-2 lg:px-10">
 	{#each products as product, i}
 		<a href="/merch/{product.id}" class={`${i % 2 === 0 ? 'pl-5' : 'pr-5'} lg:p-0`}>
@@ -46,7 +46,7 @@
 			</div>
 		</a>
 	{/each}
-</div>
+</div> -->
 
 <Swiper
 	title="In Development"
