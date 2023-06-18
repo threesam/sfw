453fbcfe717d69ac0f9c5b5286dfc7efdebf9fe6
Sam <57677512+threesam@users.noreply.github.com>
2023-06-18 14:43:11 -0400

    snaps founders to square

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -16,7 +16,7 @@
 <h3 class="mx-auto max-w-2xl px-5 font-thin">Founders</h3>
 <section class="mx-auto grid max-w-2xl gap-5 p-5 lg:grid-cols-2">
 	{#each settings.founders as { name, image }}
-		<div class="relative">
+		<div class="relative aspect-square">
 			<img src={image?.asset.url} alt="" />
 			<div class="absolute inset-0 bg-black/50" />
 			<h4 class="absolute left-3 top-3">{name}</h4>
