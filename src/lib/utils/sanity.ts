ac37f71b83cf4b80525ab1e0ccdfb72df70e20b5
Sam <57677512+threesam@users.noreply.github.com>
2023-07-01 00:52:36 -0400

    updates deprecated client creation for sanity

--- src/lib/utils/sanity.ts
+++ src/lib/utils/sanity.ts
@@ -1,8 +1,8 @@
-import sanityClient from '@sanity/client'
+import { createClient } from '@sanity/client'
 import imageUrlBuilder from '@sanity/image-url'
 import { env } from '$env/dynamic/public'
 
-const client = sanityClient({
+const client = createClient({
 	projectId: env.PUBLIC_SANITY_PROJECT_ID,
 	dataset: env.PUBLIC_SANITY_DATASET,
 	apiVersion: '2021-10-21',
