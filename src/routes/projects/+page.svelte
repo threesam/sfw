0203e0dcc198a2f65e34e8fee81b03a1b4466614
Sam <57677512+threesam@users.noreply.github.com>
2023-07-02 00:45:37 -0400

    updates projects

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -19,8 +19,13 @@
 
 <SEO title="Projects" />
 
-<h1 class="w-full px-5 pb-0 pt-10 font-display lg:text-center">All Projects</h1>
-<div class="flex flex-col pt-10 last:mb-20">
+<section class="bg-dark bg-gradient-3 relative grid h-64 w-full place-content-center">
+	<h1 class="text-bold font-display text-dark relative z-0 text-center text-3xl lg:text-5xl">
+		All Projects
+	</h1>
+</section>
+<!-- <h1 class="font-display bg-primary w-full px-5 pb-0 pt-10 lg:text-center">All Projects</h1> -->
+<div class="flex flex-col">
 	{#each sortedProjects as project, index}
 		<SideBySide {project} {index} path="/projects/" />
 	{/each}
