3039d3d7c2a688abe604a45890b44cd833557d2e
Sam <57677512+threesam@users.noreply.github.com>
2023-07-03 00:23:10 -0400

    updates header, sidebyside, and founder badging styles

--- src/lib/components/SideBySide.svelte
+++ src/lib/components/SideBySide.svelte
@@ -18,22 +18,26 @@
 		<Image src={src ?? posters?.[0].url} {alt} {caption} />
 	</div>
 	<div
-		class={`flex h-full flex-col justify-center px-5 pb-10 pt-5 ${
+		class={`flex h-full w-full flex-col items-start justify-center px-5 pb-10 pt-5 lg:items-center ${
 			index % 2 === 0 ? 'lg:text-end' : ''
 		}`}
 	>
-		<div class="block lg:hidden">
-			<Badge text={status.replace(/-/g, ' ')} />
-		</div>
-		<span class="mb-2 hidden text-sm lg:block">{status.replace(/-/g, ' ')}</span>
-		<h3 class="font-display sm:text-2xl lg:text-3xl">{title}</h3>
-		<p class="pb-3 lg:py-3">{description}</p>
+		<div
+			class="flex w-full max-w-lg flex-col justify-center lg:mx-auto lg:items-center lg:text-center"
+		>
+			<!-- <div class="block lg:hidden">
+				<Badge text={status.replace(/-/g, ' ')} />
+			</div> -->
+			<span class="mb-2 text-sm">{status.replace(/-/g, ' ')}</span>
+			<h3 class="font-display sm:text-2xl lg:text-5xl">{title}</h3>
+			<p class="pb-3 lg:py-3">{description}</p>
 
-		{#if slug}
-			<a
-				class="hover:text-primary underline underline-offset-4 transition-all duration-300 hover:underline-offset-2"
-				href={path + slug}>{buttonText}</a
-			>
-		{/if}
+			{#if slug}
+				<a
+					class="hover:text-primary underline underline-offset-4 transition-all duration-300 hover:underline-offset-2"
+					href={path + slug}>{buttonText}</a
+				>
+			{/if}
+		</div>
 	</div>
 </section>
