8d1b977e1b89de75bff46ea7b99009402a6de5a7
Sam <57677512+threesam@users.noreply.github.com>
2023-07-30 23:25:18 -0400

    tries to add ref

--- src/lib/utils/sanity/index.ts
+++ src/lib/utils/sanity/index.ts
@@ -2,6 +2,7 @@ import { env as private_env } from '$env/dynamic/private'
 import { env as public_env } from '$env/dynamic/public'
 import { createClient } from '@sanity/client'
 import type { PrintfulProduct } from '$types'
+import axios from 'axios'
 
 const client = createClient({
   projectId: public_env.PUBLIC_SANITY_PROJECT_ID,
@@ -11,7 +12,17 @@ const client = createClient({
   token: private_env.SANITY_TOKEN
 })
 
+async function uploadImage(url: string | null) {
+  if (!url) return
+
+  const { data } = await axios.get(url, { responseType: 'arraybuffer' })
+
+  return await client.assets.upload('image', Buffer.from(data, 'binary'))
+}
+
 export async function createOrReplacePrintfulProduct({ product }: { product: PrintfulProduct }) {
+  const image = await uploadImage(product.thumbnail_url)
+
   return await client.createOrReplace({
     _id: product.external_id,
     _type: 'product',
@@ -19,6 +30,9 @@ export async function createOrReplacePrintfulProduct({ product }: { product: Pri
     vendor: 'printful',
     variants: product.variants.map((variant) => ({
       _id: variant.external_id,
+      image: {
+        ref: image?._id
+      },
       name: variant.name,
       price: variant.retail_price,
       sku: variant.sku,
